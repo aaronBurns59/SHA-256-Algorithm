332567e678ec27ad329a9d2099915b7a627b995b
aaronburns59 <aaronburns59@gmail.com>
2020-02-03 19:46:23 +0000

    Set up basic c file structure

---
+++ SHA256.c
@@ -0,0 +1,9 @@
+// Aaron Burns - SHA 256 functions
+// Sourced from: https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf
+#include<stdio.h>
+#include<stdint.h>
+
+int main(int argc, char *argv[]){
+
+	return 0;
+}
