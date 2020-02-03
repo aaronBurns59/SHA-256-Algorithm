68dcff4fdd12cfafcabefe868fdc9f0c55f964b1
aaronburns59 <aaronburns59@gmail.com>
2020-02-03 20:52:05 +0000

    Created and tested Maj function, its working

--- SHA256.c
+++ SHA256.c
@@ -3,12 +3,18 @@
 #include<stdio.h>
 #include<stdint.h>
 
-//Ch is for choose, you use x to determine what bits
-// from z or y you take from 
+// Ch is for choose, you use x to determine what bits from z or y you take from 
 uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
 	// Section 4.1.2 of SHA Standard
-	// ~ is NOT in c
+	// ~ NOT in c
+	// ^ XOR in c
 	return (x & y) ^ (~x & z);
+}
+// If there are two or more 1s in any bit position between x, y and z, 
+// its going to take a 1 and if there is less thentwo 1s we get a zero in the output 
+uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
+	// Section 4.1.2 of SHA Standard
+	return (x & y) ^ (x & z) ^ (y & z);
 }
 
 int main(int argc, char *argv[]){
@@ -17,6 +23,9 @@ int main(int argc, char *argv[]){
 	uint32_t z = 0x55555555;
 
 	printf("x= %08x\ny= %08x\nz= %08x\n", x, y, z);
+
 	printf("Ch(x,y,z) = %08x\n", Ch(x,y,z));
+	printf("Maj(x,y,z) = %08x\n", Maj(x,y,z));
+	
 	return 0;
 }
