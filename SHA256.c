a82b6da7d8888a7f49d7f13ab5ee353363334ad6
aaronburns59 <aaronburns59@gmail.com>
2020-02-03 20:25:10 +0000

    Created the Ch function for the SHA 256 algorithm

--- SHA256.c
+++ SHA256.c
@@ -3,6 +3,11 @@
 #include<stdio.h>
 #include<stdint.h>
 
+uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
+	// Section 4.1.2 of SHA Standard
+	return (x & y) ^ (!x & z)
+}
+
 int main(int argc, char *argv[]){
 
 	return 0;
