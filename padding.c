7994ef57465703ce00fc7ba03e3c74dff95b8738
aaronburns59 <aaronburns59@gmail.com>
2020-02-10 19:06:56 +0000

    Created and set up padding.c file

---
+++ padding.c
@@ -0,0 +1,11 @@
+// Aaron Burns
+// Padding for SHA-256
+
+#include<stdio.h>
+// includes formatters for printing 64 bit integers
+#include<inttypes.h>
+
+int main(int argc, char* argv){
+	
+	return 0;
+}
