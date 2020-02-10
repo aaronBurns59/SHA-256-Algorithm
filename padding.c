b8fa30e8418adee41613c97e90ef5f68058cb610
aaronburns59 <aaronburns59@gmail.com>
2020-02-10 19:15:01 +0000

    Added error handling to padding.c

--- padding.c
+++ padding.c
@@ -5,7 +5,20 @@
 // includes formatters for printing 64 bit integers
 #include<inttypes.h>
 
-int main(int argc, char* argv){
+int main(int argc, char* argv[]){
+	if (argc != 2){
+		printf("Error: Expecting single filename as argument\n");
+		return 1;
+	}
 	
+	FILE *infile = fopen(argv[1], "rb");
+
+	if(!infile){
+		printf("Error: Could not open file %s", argv[1]);
+		return 1;	
+	}
+	
+	fclose(infile);
+
 	return 0;
 }
