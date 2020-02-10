4e1d033c0f7bb7f30a914b33bb558c1e189298d5
aaronburns59 <aaronburns59@gmail.com>
2020-02-10 20:53:29 +0000

    Added comments to code

--- padding.c
+++ padding.c
@@ -1,17 +1,19 @@
 // Aaron Burns
 // Padding for SHA-256
 
+// used for input and output for files 
 #include<stdio.h>
 // includes formatters for printing 64 bit integers
 #include<inttypes.h>
 
 uint64_t NoZerosBytes(uint64_t nobits){
 	// the number of bits from the file retrieved from the read file block of code ()
-	// ULL : Unsigned Long Long integer 
+	// ULL : Unsigned Long Long integer (Makes sure c saves var as a 64 bit integer)
+	// The final output needs to be a multiple of 512 bits
 	uint64_t result = 512ULL - (nobits % 512ULL);
 
 	// find out if there is enough room in the last block to do the padding or will a new block be needed
-	if (result < 65)// 65 64(length integer and the 1)
+	if (result < 65)// 65 (length of integer and the 1)
 		result += 512;
 	
 	// get the number of 0 bytes needed to pad between the 1 and the 64 bit integer printed at the end
@@ -22,18 +24,23 @@ uint64_t NoZerosBytes(uint64_t nobits){
 int main(int argc, char* argv[]){
 	if (argc != 2){
 		printf("Error: Expecting single filename as argument\n");
+		// if main returns anything other then 0 that indicates an error
 		return 1;
 	}
 	
+	// declare for reading file
 	FILE *infile = fopen(argv[1], "rb");
 
 	if(!infile){
 		printf("Error: Could not open file %s\n", argv[1]);
 		return 1;	
 	}
-
+	
+	// read a byte at a time from the input file
 	uint8_t b;
+	// keep track of the number of bits read from the input
 	uint64_t nobits;	
+	
 	//  '&' is the address of b not b itself
 	// allows us to overwrite the value stored in the address b
 	// It reads the file 1 byte at a time
@@ -53,6 +60,7 @@ int main(int argc, char* argv[]){
 	// prints the number of bits to the screen 
 	printf("%016" PRIx64 "\n", nobits);
 	
+	// need to close the file when finished with it
 	fclose(infile);
 
 	return 0;
