a619e93b943670fbba0a30adb54bce030896805b
aaronburns59 <aaronburns59@gmail.com>
2020-02-17 19:37:49 +0000

    Created the unions.c file and set up the basics for it

---
+++ unions.c
@@ -0,0 +1,16 @@
+// Working with unions in c
+// Aaron Burns
+
+#include <stdio.h>
+#include <inttypes.h>
+
+union endian{
+	// takes up 2 bytes
+	uint8_t ei[2];
+	uint16_t ai;
+};
+
+int main(int argc, char* argv ){
+	
+	return 0;
+}
